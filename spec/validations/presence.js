01c04d7768ca678ff90765743989e18f45316e76
Aditya Manohar <mail@adityamanohar.com>
2013-03-30 15:35:28 -0700

    Added tests for presence

---
+++ spec/validations/presence.js
@@ -0,0 +1,37 @@
+describe("Test for presence", function(){
+
+	var User, user;
+
+	beforeEach(function(){
+		User = Backbone.Model.extend({
+			validates: {
+				first_name: {
+					presence: true
+				}
+			}
+		});
+		
+		user  = new User();
+	});
+	
+	it("should not allow empty strings", function(){
+		user.set({first_name: ""});
+		expect(user.isValid()).toBeFalsy();
+	});
+	
+	it("should not allow strings with only spaces", function(){
+		user.set({first_name: "   "});
+		expect(user.isValid()).toBeFalsy();
+	});
+	
+	it("should not allow NULL values", function(){
+		user.set({first_name: null});
+		expect(user.isValid()).toBeFalsy();
+	});
+	
+	it("should not allow UNDEFINED values", function(){
+		user.set({first_name: undefined});
+		expect(user.isValid()).toBeFalsy();
+	});
+	
+})	
