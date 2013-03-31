4bafd3ace6805026d4b3cef8ef230d8c5abe5f05
Aditya Manohar <mail@adityamanohar.com>
2013-03-31 00:57:57 -0700

    Added tests for type EMAIL

---
+++ spec/validations/email.js
@@ -0,0 +1,32 @@
+describe("Test for type EMAIL", function(){
+	
+	var User = Backbone.Model.extend({
+		validates: {
+			email: {
+				type: "email"
+			}
+		}
+	}),
+	user;
+	
+	beforeEach(function(){
+		user = new User();
+	});
+	
+	it("should not allow empty strings", function(){
+		user.set({email: ""});
+		expect(user.isValid()).toBeFalsy();
+	});
+	
+	it("should not allow only whitespace strings", function(){
+		user.set({email: "    "});
+		expect(user.isValid()).toBeFalsy();
+	});
+	
+	it("should return custom error message", function(){
+		user.validates.email.msg = "This is a bogus email";
+		user.set({email: ""});
+		user.isValid();
+		expect(user.validationError.email.type).toEqual("This is a bogus email");
+	});
+})
